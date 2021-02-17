1243fa5a399e1989f30c9e193a3b0923c50920ad
F.Prieto <felipe00@live.com.mx>
2021-02-17 11:35:43 -0600

    Update Script-insertar-csv.py

--- Script-insertar-csv.py
+++ Script-insertar-csv.py
@@ -8,7 +8,13 @@ class insert():
         print(df)
         engine = create_engine('postgresql://urbvan:root2022@database-postgress.c295zanvccq2.us-east-1.rds.amazonaws.com:5432/postgres')
         for df in pd.read_csv("client_table.csv",chunksize=30000):
+            i = 0
+            j = 0
+            df = df.rename(columns={c: c.replace(' ', '') for c in df.columns})
+            df.index += j
+            i+=1
             df.to_sql('client_table.csv',engine, index=False, if_exists='append')
+            j = df.index[-1] + 1
    
 
     def insert_driver(self):
@@ -16,21 +22,40 @@ class insert():
         print(df2)
         engine2 = create_engine('postgresql://urbvan:root2022@database-postgress.c295zanvccq2.us-east-1.rds.amazonaws.com:5432/postgres')
         for df2 in pd.read_csv("driver_table.csv",chunksize=1000):
+            i = 0
+            j = 0
+            df2 = df.rename(columns={c: c.replace(' ', '') for c in df.columns})
+            df.index += j
+            i+=1
             df2.to_sql('driver_table.csv',engine2, index=False, if_exists='append')
+            j = df2.index[-1] + 1
             
     def insert_reservation(self):
         df3 = pd.read_csv('reservation_table.csv')
         print(df3)
         engine3 = create_engine('postgresql://urbvan:root2022@database-postgress.c295zanvccq2.us-east-1.rds.amazonaws.com:5432/postgres')
         for df3 in pd.read_csv('reservation_table.csv', chunksize=3106171):
+             i = 0
+             j = 0
+             df3 = df.rename(columns={c: c.replace(' ', '') for c in df.columns})
+             df.index += j
+             i+=1
+            
             df3.to_sql('reservation_table.csv',engine3,index=False,if_exists='append')
+             j = df3.index[-1] + 1
             
     def insert_trip(self):
         df4 = pd.read_csv('trip_table.csv')
         print(df4)
         engine4 = create_engine('postgresql://urbvan:root2022@database-postgress.c295zanvccq2.us-east-1.rds.amazonaws.com:5432/postgres')
         for df4 in pd.read_csv('trip_table.csv',chunksize=175292):
+             i = 0
+             j = 0
+             df4 = df.rename(columns={c: c.replace(' ', '') for c in df.columns})
+             df4.index += j
+             i+=1
             df4.to_sql('trip_table.csv',engine4,index=False,if_exists='append')
+            j = df4.index[-1] + 1
     def insert_with_spark(self):
         myData = spark.read.format("csv").option("header","true").load("client_table.csv")
         url = 'postgresql:database-postgress.c295zanvccq2.us-east-1.rds.amazonaws.com
