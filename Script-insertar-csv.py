f0e4618ce0590ac7632be6ff257d361a82b677d3
F.Prieto <felipe00@live.com.mx>
2021-02-17 03:12:27 -0600

    Update Script-insertar-csv.py

--- Script-insertar-csv.py
+++ Script-insertar-csv.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 class insert():
-    def insert_client():
+    def insert_client(self):
         df = pd.read_csv ('client_table.csv')
         print(df)
         engine = create_engine('postgresql://urbvan:root2022@database-postgress.c295zanvccq2.us-east-1.rds.amazonaws.com:5432/postgres')
@@ -11,21 +11,21 @@ class insert():
             df.to_sql('client_table.csv',engine, index=False, if_exists='append')
    
 
-    def insert_driver():
+    def insert_driver(self):
         df2 = pd.read_csv ('driver_table.csv')
         print(df2)
         engine2 = create_engine('postgresql://urbvan:root2022@database-postgress.c295zanvccq2.us-east-1.rds.amazonaws.com:5432/postgres')
         for df2 in pd.read_csv("driver_table.csv",chunksize=1000):
             df2.to_sql('driver_table.csv',engine2, index=False, if_exists='append')
             
-    def insert_reservation():
+    def insert_reservation(self):
         df3 = pd.read_csv('reservation_table.csv')
         print(df3)
         engine3 = create_engine('postgresql://urbvan:root2022@database-postgress.c295zanvccq2.us-east-1.rds.amazonaws.com:5432/postgres')
         for df3 in pd.read_csv('reservation_table.csv', chunksize=3106171):
             df3.to_sql('reservation_table.csv',engine3,index=False,if_exists='append')
             
-    def insert_trip():
+    def insert_trip(self):
         df4 = pd.read_csv('trip_table.csv')
         print(df4)
         engine4 = create_engine('postgresql://urbvan:root2022@database-postgress.c295zanvccq2.us-east-1.rds.amazonaws.com:5432/postgres')
